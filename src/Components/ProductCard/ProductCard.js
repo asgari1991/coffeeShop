e53f1a872fe887688e9e17f8530e1aab013bce87
Mohammad <m.asgari91@gmail.com>
2024-08-19 19:56:06 +0430

    designing product Card content 1

--- src/Components/ProductCard/ProductCard.js
+++ src/Components/ProductCard/ProductCard.js
@@ -2,8 +2,25 @@ import React from 'react'
 
 export default function ProductCard() {
   return (
-    <div className='p-20 bg-white dark:bg-zinc-700 rounded-2xl shadow-normal '>
-      
+    <div className='p-5 bg-white dark:bg-zinc-700 rounded-2xl shadow-normal '>
+      <div className=' relative mb-5'>
+        <img src="./images/products/p1.png" alt="p1" />
+        <span className=' absolute top-1.5 right-1.5 block h-[30px] leading-[34px] bg-orange-300 font-DanaDemiBold text-white dark:text-zinc-700 px-3.5 rounded-full'>12%</span>
+      </div>
+      <h5
+      >قهوه ترک بن مانو مقدار 250 گرم
+         خط دوم اسم طولانی
+         </h5>
+      <div>
+        <div>
+            <span>154,000 </span>
+            <span>تومان</span>
+        </div>
+        <div>
+        <span>175,000 </span>
+            <span>تومان</span>
+        </div>
+      </div>
     </div>
   )
 }
